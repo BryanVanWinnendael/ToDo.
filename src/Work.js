6c3c73a18fb5b78803351a52aa631a0dd0eb4ea9
Bryan Van Winnendael <bryan.vanwinnendael@student.ucll.be>
2021-10-22 19:56:17 +0200

    work

---
+++ src/Work.js
@@ -0,0 +1,45 @@
+import React from "react";
+import './App.css';
+
+
+
+function Work() {
+
+  return (
+   
+    <div className="workbox" style={{
+      width:"100%",
+      height:"100vh",
+      display:"flex",
+      flexDirection:"column"
+     
+    }}>
+      {/* <h1
+      style={{
+        paddingLeft:"5px",
+        paddingRight:"5px",
+        paddingBottom:"2px",
+        color:"white",
+        backgroundColor:"#282c34",
+        borderRadius:"15px",
+        boxShadow: "5px 5px 8px rgb(136 136 136 / 38%)",
+      }}
+      >Work </h1> */}
+      <div style={{
+        width:"auto",
+        
+        
+    
+      }}>
+      <iframe style={{
+        borderRadius:"25px",
+        boxShadow: "5px 5px 8px rgb(136 136 136 / 38%)"
+      }}
+      id="iframeId" width="auto" height="300px" src="https://www.youtube.com/embed/videoseries?list=PL6NdkXsPL07IOu1AZ2Y2lGNYfjDStyT6O" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      </div>
+
+  );
+}
+
+export default Work;
