b55bbaac8057481e6936666e79f7cdc92bdf61a1
Bryan Van Winnendael <bryan.vanwinnendael@student.ucll.be>
2021-11-02 16:19:36 +0100

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './App.css';
 import Home from "./Home"
-import Work from "./Work"
-import Nav from "./components/Nav";
+
+
 import {Route} from 'react-router-dom'
 
 
@@ -11,10 +11,10 @@ function App() {
   return (
    
     <div className="App">
-        <Nav/>
+
 
         <Route exact path="/" component={Home} />
-        <Route exact path="/work" component={Work} />
+    
     </div>
   );
 }
