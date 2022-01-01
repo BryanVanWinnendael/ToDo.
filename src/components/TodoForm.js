34c9cd4cb39295a7c39e9f4b5e8c81639efe5173
Bryan Van Winnendael <bryan.vanwinnendael@student.ucll.be>
2022-01-01 15:53:36 +0100

    input fix

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import firebase from "../util/firebase";
 import { Button } from '@mui/material';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
@@ -6,58 +6,47 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import TextField from '@mui/material/TextField';
 import CalendarPicker from '@mui/lab/CalendarPicker';
 import PropTypes from 'prop-types';
+import { makeStyles } from "@material-ui/core/styles";
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200,
-    color:'red'
-  },
-
-  cssLabel: {
-    color : 'red !important'
-  },
-
-  cssOutlinedInput: {
-    '&$cssFocused $notchedOutline': {
-      borderColor: `${theme.palette.primary.main} !important`,
+const useStyles = makeStyles({
+  root: {
+    // input label when focused
+    "& .MuiInputLabel-root": {
+      color: "var(--text-color)"
+    },
+    // focused color for input with variant='standard'
+    "& .MuiInput-underline:before": {
+      borderBottomColor: "var(--text-color)"
+    },
+    
+    "& .MuiInput-input":{
+      color:"var(--text-color)"
+    },
+    "& .MuiInput-root:hover:not(.Mui-disabled):before":{
+      borderBottomColor: "var(--text-color)"
     }
-  },
-
-  cssFocused: {},
-
-  notchedStandard: {
-    borderWidth: '1px',
-    borderColor: 'red !important'
-  },
-
+   
+   
+  }
 });
 
-class TodoForm extends  React.Component{
-  state = {
-    name: 'InputMode',
-  }
-
+function TodoForm(){
+  const [title, setTitle] = useState("");
+  const [date, setDate] = React.useState(null);
+  const classes = useStyles();
   
 
-  handleChange = (e) => {
-      // setTitle(e.target.value);
+  const handleChange = (e) => {
+      setTitle(e.target.value);
     };
     
-  createTodo = (event) => {
+  const createTodo = (event) => {
       event.preventDefault();
       
       const todoRef = firebase.database().ref("Todo");
       var todo = {}
-      const title = ""
-      const date = ""
-
+     
       if(date === null){
         todo = {
           title,
@@ -69,93 +58,51 @@ class TodoForm extends  React.Component{
         todo = {
           title,
           complete: false,
-          date:this.convertDate(date)
+          date:convertDate(date)
         };
       }
      
       todoRef.push(todo);
-      // setTitle("");   
-      // setDate(null)
+      setTitle("");   
+      setDate(null)
     };
     
-    convertDate(dateString) {
+  function convertDate(dateString) {
       var date = new Date(dateString);
       return date.getDate()+"/"+(date.getMonth() + 1)+"/"+date.getFullYear();
   }
   
-  render(){
-    const { classes } = this.props;
-
   return(
-        // <form onSubmit={this.createTodo} >
-          <form  >
+        <form onSubmit={createTodo} >
         
        
         <div>
           <LocalizationProvider dateAdapter={AdapterDateFns} >
-                {/* <CalendarPicker date={date} onChange={(newDate) =>setDate(newDate)} /> */}
-                <CalendarPicker />
-
+                <CalendarPicker date={date} onChange={(newDate) =>setDate(newDate)} />
           </LocalizationProvider>
-          {/* {date && (
+          {date && (
             <p style={{
               marginTop:"0",
               color:"#1976d2",
               fontWeight:"bold"
-            }}>Date picked: {this.convertDate(date)} </p>
+            }}>Date picked: {convertDate(date)} </p>
           )}
-         */}
+        
         </div>
       
 
         <div >
           <TextField id="standard-basic"  variant="standard" label="Add task"
           type="text"
-          onChange={this.handleChange}  
-          value={"title"} 
+          onChange={handleChange}  
+          value={title} 
           name="task"
           required
-          className={classes.textField}
+          className={classes.root}
           
-          InputLabelProps={{
-            classes: {
-              root: classes.cssLabel,
-              focused: classes.cssFocused,
-            },
-          }}
-          InputProps={{
-            classes: {
-              root: classes.cssOutlinedInput,
-              focused: classes.cssFocused,
-              notchedOutline: classes.notchedOutline,
-            },
-         
-          }}
+        
           
           />
-           <TextField
-          id="standard-name"
-          label="Name"
-          className={classes.textField}
-          value={this.state.name}
-          onChange={this.handleChange('name')}
-          margin="normal"
-          variant="outlined"
-          InputLabelProps={{
-            classes: {
-              root: classes.cssLabel,
-              focused: classes.cssFocused,
-            },
-          }}
-          InputProps={{
-            classes: {
-              root: classes.cssOutlinedInput,
-              focused: classes.cssFocused,
-              notchedOutline: classes.notchedOutline,
-            },
-            inputMode: "numeric"
-          }}
-        />
           <Button variant="contained" type="submit" style={{
             margin:"10px",
           }} startIcon={
@@ -179,13 +126,8 @@ class TodoForm extends  React.Component{
        
         </form>
     );
-  }
-
 }
 
-TodoForm.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 
-export default withStyles(styles)(TodoForm)
+export default TodoForm;
