06c024c17ddf03427804ba3de7ace108966431fc
Bryan Van Winnendael <bryan.vanwinnendael@student.ucll.be>
2022-02-23 13:33:29 +0100

    env

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCXNyXcJEeLGQWDjvFXWfBqrwn2ANyOGgY",
-    authDomain: "todo-d6484.firebaseapp.com",
-     databaseURL:"https://todo-d6484-default-rtdb.europe-west1.firebasedatabase.app/",
-    projectId: "todo-d6484",
-    storageBucket: "todo-d6484.appspot.com",
-    messagingSenderId: "189561472722",
-    appId: "1:189561472722:web:2398d70e3f2df9e7a68f0b"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
   
 // Initialize Firebase
